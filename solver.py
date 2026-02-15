25b4c03e946f776d2f407b6f613c8ad91f422663
ary4n-na1r <aryan.nair389@gmail.com>
2026-02-15 20:56:14 +0000

    Update solver.py

--- solver.py
+++ solver.py
@@ -1,11 +1,19 @@
-maze55 = [
+user_maze = [
   ["S","0","0","0","1"],
   ["1","1","0","0","1"],
   ["0","0","0","0","1"],
   ["1","0","1","0","E"],
   ["1","1","1","0","E"]
 ]
-for i in maze55:
-  for x in i:
-    if x == "S":
-      print(maze55[i][x])
+
+def find_start_end(maze):
+  for row in range(len(maze)):
+    for col in range(len(maze[0])):
+      if maze[row][col] == "S":
+        start = (row, col)
+      if maze[row][col] == "E":
+        end = (row, col)
+  print("Start: ",start)
+  print("End: ",end)
+
+find_start_end(user_maze)
